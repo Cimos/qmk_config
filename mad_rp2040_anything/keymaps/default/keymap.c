711e0f1787c8f2d80dc52ab7511301cb8d35d732
Madman <si.maddison@icloud.com>
2025-11-15 19:54:27 +1100

    Configures keyboard for 4x4 matrix
    Updates keyboard configuration to support a 4x4 matrix layout.
    This includes modifying the matrix pins, layouts, and RGB matrix
    settings to accurately reflect the new matrix arrangement.
    Also updates keymaps to match 4x4 layout.

--- mad_rp2040_anything/keymaps/default/keymap.c
+++ mad_rp2040_anything/keymaps/default/keymap.c
@@ -10,7 +10,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [0] = LAYOUT(
-  KC_1, KC_2
+    KC_1, KC_5, KC_9, KC_4,
+    KC_2, KC_6, KC_1, KC_5,
+    KC_3, KC_7, KC_2, KC_6,
+    KC_4, KC_8, KC_3, KC_7
 )
 };
 
--- mad_rp2040_anything/keymaps/via/keymap.c
+++ mad_rp2040_anything/keymaps/via/keymap.c
@@ -6,11 +6,14 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* 0
- *  |   1  |   2  |
+ *
  */
 
  [0] = LAYOUT(
-  KC_1, KC_2
+  KC_1, KC_5, KC_9, KC_4,
+  KC_2, KC_6, KC_1, KC_5,
+  KC_3, KC_7, KC_2, KC_6,
+  KC_4, KC_8, KC_3, KC_7
 )
 };
 
